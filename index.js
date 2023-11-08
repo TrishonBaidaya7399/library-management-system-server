6711e191c1504dc26d30da48c663bfbe3b2c86a6
TrishonBaidaya7399 <githubtrishon@gmail.com>
2023-11-08 21:34:18 +0600

    vercel.json file added

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 //hv2ReAvSCVR00jYS
 //BookShelf
-
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wlvfdkk.mongodb.net/?retryWrites=true&w=majority`;
 const uri = "mongodb+srv://BookShelf:hv2ReAvSCVR00jYS@cluster0.wlvfdkk.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
